530544fdc102750483977ccbc2c7ccd5b978a51d
khalil224 <khalilislamju@gmail.com>
2022-05-23 21:36:23 +0600

    portfolio added

--- src/routes/MyPortfolio.js
+++ src/routes/MyPortfolio.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 const MyPortfolio = () => {
     return (
-        <div className='mt-24'>
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <figure class="px-10 pt-10">
-                    <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" class="rounded-xl" />
-                </figure>
-                <div class="card-body items-center text-center">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
+        <div className='mt-24 flex justify-center items-center'>
+            <div class="card w-96 bg-base-100 shadow-xl ">
+
+                <div class="card-body items-center text-center ml-6 mr-6">
+                    <h2 class="card-title text-3xl text-primary uppercase underline">My Portfolio</h2>
+
+                    <h3 className='text-xl font-bold'>Name: Muhammad Khalil Islam</h3>
+                    <p className='font-bold'>Email: <span>khalilislamju@gmail.com</span>
+                    </p>
+                    <p className='font-bold'> Educational Background: <span>Currently studying B.Sc. honours  major in Mathematics at Jahangirnagar University, Savar, Dhaka.  </span></p>
+                    <p className='font-bold'>List of technologies: C, C++, MATLAB, HTML,CSS,Bootstrap,Tailwind Css, Javascript, React, NodeJs,Mongodb,Data Structure etc. </p>
+                    <p className='font-bold'>Website Link: </p>
                 </div>
             </div>
         </div>
