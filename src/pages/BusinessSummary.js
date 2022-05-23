3f61f5166c316d3d0c1956c2c0f4d5c89de64a58
khalil224 <khalilislamju@gmail.com>
2022-05-23 07:22:32 +0600

    route and page created

---
+++ src/pages/BusinessSummary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusinessSummary = () => {
+    return (
+        <div>
+            <h2>this is business summary page</h2>
+        </div>
+    );
+};
+
+export default BusinessSummary;
