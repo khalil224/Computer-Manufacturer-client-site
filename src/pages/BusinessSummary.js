7865ace2ef7e367fdc458872e99a832d0f117e7b
khalil224 <khalilislamju@gmail.com>
2022-05-25 21:10:40 +0600

    business summary creating

--- src/pages/BusinessSummary.js
+++ src/pages/BusinessSummary.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import review from '../images/Business Summary/reviews.png'
+
 const BusinessSummary = () => {
     return (
         <div className='mt-24 text-center'>
             <h2 className='text-5xl text-primary font-bold mb-3'>THOUSAND BUSINESS TRUST US</h2>
             <p className='text-2xl'>TRY TO UNDERSTAND USER EXPECTATION</p>
-            <p></p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-24'>
+                <div className=''>
+                    <img src={review} alt="" className='h-32 w-32' />
+                    <p className='text-2xl'>30K+ Reviews</p>
+                </div>
+                <div>
+                    <img src={review} alt="" className='h-32 w-32' />
+                    <p className='text-2xl'>30K+ Reviews</p>
+                </div>
+                <div>
+                    <img src={review} alt="" className='h-32 w-32' />
+                    <p className='text-2xl'>30K+ Reviews</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/PurchasePage.js
+++ src/pages/PurchasePage.js
@@ -9,8 +9,35 @@ const PurchasePage = () => {
     const [user, loading, error] = useAuthState(auth);
 
     const [tool, setTool] = useState([]);
+    const [Count, setCount] = useState(150)
+    console.log(user)
 
-    console.log(tool)
+    const handleBooking = (e) => {
+        e.preventDefault();
+
+        const order = {
+
+        }
+
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+
+
+            })
+
+
+
+
+    }
+
     useEffect(() => {
         // const url = `http://localhost:5000/tool/${id}`;
         // fetch(url)
@@ -22,17 +49,42 @@ const PurchasePage = () => {
         })();
     }, []);
 
+    const increment = () => {
+        setCount(Count + 1);
+
+    }
+    const decrement = () => {
+        setCount(Count - 1);
+    }
+
     return (
 
-        <div class="hero min-h-screen bg-base-100 mt-24 mb-24">
-            <div class="hero-content flex-col lg:flex-row ">
-                <img src={tool.img} alt='' class="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 class="text-3xl font-bold">{tool.Name}</h1>
-                    <p class="py-3 font-semibold">{tool.description}</p>
-                    <p className='font-bold'>Price:{tool.Price}</p>
-                    <p className='font-bold'>Minimum Order Quantity: {tool.minimumOrderQuantity}</p>
-                    <p className='font-bold'>Available Quantity: {tool.availableOrderQuantity}</p>
+        <div className='mb-24'>
+            <div class="hero min-h-screen bg-base-100 mt-24 ">
+                <div class="hero-content flex-col lg:flex-row ">
+                    <img src={tool.img} alt='' class="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 class="text-3xl font-bold">{tool.Name}</h1>
+                        <p class="py-3 font-semibold">{tool.description}</p>
+                        <p className='font-bold'>Price:{tool.Price}</p>
+                        <p className='font-bold'>Minimum Order Quantity: {tool.minimumOrderQuantity}</p>
+                        <p className='font-bold'>Available Quantity: {tool.availableOrderQuantity}</p>
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-center items-center'>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <form onSubmit={handleBooking} >
+                            <h2 className='text-center my-3 text-3xl font-bold text-primary underline'>PURCHASE FIELD</h2>
+                            <div className='text-center '><span><button className='btn btn-primary mr-3' onClick={increment}>Increase <br />Order <br />Quantity</button>{Count}<button className='btn btn-primary ml-3' onClick={decrement}>Decrease <br />Order <br />Quantity</button></span></div>
+                            <input type="text" name="name" disabled value={user?.displayName || ''} class="input input-bordered w-full max-w-lg my-3" />
+                            <input type="text" name='email' disabled value={user?.email || ''} class="input input-bordered w-full max-w-lg my-3" />
+                            <input type="text" name='phone' placeholder="Phone Number" class="input input-bordered w-full max-w-lg my-3" />
+                            {<input type="text" name='address' placeholder="Your Address" class="input input-bordered w-full max-w-lg my-3" />}
+                            <input type="submit" value="SUBMIT" class="btn btn-primary w-full max-w-lg my-3" />
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
